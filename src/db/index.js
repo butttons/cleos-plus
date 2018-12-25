89736e59b657910cc4288949d7ebfa52a1437443
Yash Baheti <zilllionaire@gmail.com>
2018-12-25 16:50:49 +0530

    added ID based contract items

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,23 @@
 const db = require('./low');
+const shortid = require('shortid');
 const handles = {
     list: (table = 'accounts') => db.get(table).value(),
     add: (table = 'accounts', data = {}) => db.get(table).push(data).write(),
     exists: (table = 'accounts', where = { name: 'zolotoken' }) => db.get(table).find(where).value(),
-    updateContract: ({ account, contract }) => {
+    updateContract: ({ account, contract, result, abi }) => {
         const exists = db.get('contracts').keys().value().includes(account);
         if (!exists) {
             db.set(`contracts.${account}`, []).write();
         }
-        db.get(`contracts.${account}`).push(contract).write();
+        const data = {
+            id: shortid.generate(),
+            time: Date.now(),
+            contract,
+            abi,
+            result
+        };
+        db.get(`contracts.${account}`).push(data).write();
+        db.get('accounts').find({ name: account }).assign({ currentContract: data.id }).write();
     },
     listAccounts: () => {
         const accounts = handles.list();
@@ -21,6 +30,10 @@ const handles = {
                 contracts_deployed: accountContracts
             };
         });
+    },
+    getContract: (contractName) => {
+        const contract = db.get(`contracts.${contractName}`).sortBy('time').reverse().take(1).value();
+        return contract[0];
     },
     setConfig: (config) => db.set('config', config).write(),
     eosConfig: db.get('config').value()
