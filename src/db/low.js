e851eee0bb63ddbf4776db1e777601449895b43d
Yash Baheti <zilllionaire@gmail.com>
2018-12-25 16:50:30 +0530

    db defaults

--- src/db/low.js
+++ src/db/low.js
@@ -6,6 +6,24 @@ const adapter = new FileSync(path.resolve(__dirname, 'db.json'));
 const db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ accounts: [], contracts: {}, config: {} }).write();
+db
+    .defaults({
+        accounts: [],
+        contracts: {},
+        config: {}
+    })
+    .write();
+if (!db.get('config').value()) {
+    db
+        .set('config', {
+            api: 'http://localhost:7777',
+            contractDir: '',
+            owner: {
+                name: 'eosio',
+                key: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+            }
+        })
+        .write();
+}
 
 module.exports = db;
